6740686b0b97118aa4678807a9edbf4c6aaf5ce8
Perry Smith <pedz@easesoftware.com>
2014-06-05 13:32:03 -0500

    various tweaks and updates

--- pedz/pedz.el
+++ pedz/pedz.el
@@ -26,6 +26,15 @@
   (define-key tmap "\C-u" 'backward-kill-line))
 
 ;;
+;; toggle the case fold search flag
+;;
+(defun toggle-buffer-case-fold-search ()
+  "Toggles the case fold search flag for the local buffer"
+  (interactive)
+  (message "case-fold-search is now %s"
+           (prin1-to-string (setq case-fold-search (not case-fold-search)))))
+
+;;
 ;; My own map of things is in this map and I hook the map to \C-\\ for now
 ;;
 (defvar personal-map (make-sparse-keymap)
@@ -49,7 +58,7 @@
                                    (interactive)
                                    (recenter 0))))
 (define-key personal-map "l" 'list-all-matching-lines)
-(define-key personal-map "\C-c" 'toggle-case-fold-search)
+(define-key personal-map "\C-c" 'toggle-buffer-case-fold-search)
 
 (define-key global-map "\C-\\" personal-map)
 
@@ -142,6 +151,55 @@
 
 ;; Add shift-<arrow key> to move between windows
 (windmove-default-keybindings)
+
+
+(require 'url)
+
+(defun mac-ae-get-url (event)
+  "Open the URL specified by the Apple event EVENT.
+Currently the `mailto' and `txmt' schemes are supported."
+  (interactive "e")
+  (let* ((ae (mac-event-ae event))
+	 (the-text (mac-ae-text ae))
+	 (parsed-url (url-generic-parse-url the-text))
+	 (the-url-type (url-type parsed-url)))
+    (case (intern the-url-type)
+      (mailto
+       (progn
+	 (url-mailto parsed-url)
+	 (select-frame-set-input-focus (selected-frame))))
+      (txmt
+       (let* ((not-used (string-match "txmt://open\\?url=file://\\([^&]*\\)\\(&line=\\([0-9]*\\)\\)?" the-text))
+	      (file-name (match-string 1 the-text))
+	      (lineno (match-string 3 the-text)))
+	 (if (null file-name)
+	     (message "Bad txmt URL: %s" the-text)
+	   (find-file file-name)
+	   (goto-char (point-min))
+	   (if lineno
+	       (forward-line (1- (string-to-number lineno))))
+	   (select-frame-set-input-focus (selected-frame)))))
+      (t (mac-resume-apple-event ae t)))))
+
+
+;;;
+;;; Rules to help with alignments in Ruby mode
+;;;
+(defvar align-rules-list)
+(eval-after-load 'align
+  '(dolist (ar '((ruby-migration-column-names
+		  (regexp . "\\s-*t\\.\\(\\s_\\|\\sw\\)+\\( +\\)")
+		  (group . 2)
+		  (column . 20)
+		  (separate . "\\<\\(do\\|end\\)\\>")
+		  (modes . (list 'ruby-mode))
+		  )
+		 (ruby-migration-column-attributes
+		  (regexp . ",\\(\\s-+\\)")
+		  (repeat . t)
+		  (modes . (list 'ruby-mode))
+		  )
+		 )) (add-to-list 'align-rules-list ar t)))
 
 
 ;;;### (autoloads (prvm-activate) "prvm" "prvm.el" (21138 27996 0
