5c6b313dc511354cbb5eb41a943d2677b497f1a8
Perry Smith <pedz@easesoftware.com>
2016-12-26 08:05:21 -0600

    Position display on spitfire and Mac

--- customize.el
+++ customize.el
@@ -5,6 +5,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ac-modes
+   (quote
+    (emacs-lisp-mode lisp-mode lisp-interaction-mode slime-repl-mode go-mode java-mode malabar-mode clojure-mode clojurescript-mode scala-mode scheme-mode ocaml-mode tuareg-mode coq-mode haskell-mode agda-mode agda2-mode perl-mode cperl-mode python-mode ruby-mode lua-mode tcl-mode ecmascript-mode javascript-mode js-mode js2-mode php-mode css-mode scss-mode less-css-mode makefile-mode sh-mode fortran-mode f90-mode ada-mode xml-mode sgml-mode web-mode ts-mode sclang-mode verilog-mode qml-mode apples-mode)))
  '(ansi-color-for-comint-mode t)
  '(ansi-color-names-vector
    ["black" "red" "green" "gold" "blue" "magenta" "darkturquoise" "dark green"])
@@ -29,13 +32,18 @@
  '(inhibit-startup-screen t)
  '(initial-frame-alist
    (cond
-    ((string-match "spitfire.*:3" (car (x-display-list)))
+    ((string-match "spitfire.*:3"
+		   (car
+		    (x-display-list)))
      (quote
       ((top . 0)
        (left . 0)
        (width . 90)
        (height . 99))))
-    ((string= (car (x-display-list)) "Mac")
+    ((string=
+      (car
+       (x-display-list))
+      "Mac")
      (quote
       ((top . 23)
        (left . 607)
