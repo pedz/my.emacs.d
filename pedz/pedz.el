529a22930479633094bc7552b05ff4a4be62f1e7
Perry Smith <pedz@easesoftware.com>
2016-12-31 19:40:08 -0600

    Fix more compile errors
    The Mac specific events are not defined except on the Mac.  Added
    conditions to prevent byte compile warnings.

--- pedz/pedz.el
+++ pedz/pedz.el
@@ -196,31 +196,36 @@ Key bindings:
 
 (require 'url)
 
-(defun mac-ae-get-url (event)
-  "Open the URL specified by the Apple event EVENT.
+(if (and
+     (fboundp 'mac-event-ae)
+     (fboundp 'mac-ae-text)
+     (fboundp 'mac-resume-apple-event))
+    (defun mac-ae-get-url (event)
+      "Open the URL specified by the Apple event EVENT.
 Currently the `mailto' and `txmt' schemes are supported."
-  (interactive "e")
-  (let* ((ae (mac-event-ae event))
-	 (the-text (mac-ae-text ae))
-	 (parsed-url (url-generic-parse-url the-text))
-	 (the-url-type (url-type parsed-url)))
-    (cl-case (intern the-url-type)
-      (mailto
-       (progn
-	 (url-mailto parsed-url)
-	 (select-frame-set-input-focus (selected-frame))))
-      (txmt
-       (let* ((not-used (string-match "txmt://open\\?url=file://\\([^&]*\\)\\(&line=\\([0-9]*\\)\\)?" the-text))
-	      (file-name (match-string 1 the-text))
-	      (lineno (match-string 3 the-text)))
-	 (if (null file-name)
-	     (message "Bad txmt URL: %s" the-text)
-	   (find-file file-name)
-	   (goto-char (point-min))
-	   (if lineno
-	       (forward-line (1- (string-to-number lineno))))
-	   (select-frame-set-input-focus (selected-frame)))))
-      (t (mac-resume-apple-event ae t)))))
+      (interactive "e")
+      (let* ((ae (mac-event-ae event))
+	     (the-text (mac-ae-text ae))
+	     (parsed-url (url-generic-parse-url the-text))
+	     (the-url-type (url-type parsed-url)))
+	(cl-case (intern the-url-type)
+	  (mailto
+	   (progn
+	     (url-mailto parsed-url)
+	     (select-frame-set-input-focus (selected-frame))))
+	  (txmt
+	   (let* ((not-used (string-match "txmt://open\\?url=file://\\([^&]*\\)\\(&line=\\([0-9]*\\)\\)?" the-text))
+		  (file-name (match-string 1 the-text))
+		  (lineno (match-string 3 the-text)))
+	     (if (null file-name)
+		 (message "Bad txmt URL: %s" the-text)
+	       (find-file file-name)
+	       (goto-char (point-min))
+	       (if lineno
+		   (forward-line (1- (string-to-number lineno))))
+	       (select-frame-set-input-focus (selected-frame)))))
+	  (t (mac-resume-apple-event ae t)))))
+  )
 
 
 ;;;### (autoloads (prvm-activate) "prvm" "prvm.el" (21138 27996 0
