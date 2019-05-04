bcd895fcea74d2c740a8c9db7ae3542fc47e5062
Perry Smith <pedz@easesoftware.com>
2019-05-04 10:06:38 -0500

    First version with clean slate...
    Lets see how this works out.

--- init.el
+++ init.el
@@ -22,105 +22,10 @@
 (dolist (dir '( "el-get/el-get" "pedz" ))
   (add-to-list 'load-path (expand-file-name dir user-emacs-directory)))
 
-;; ;; Need this very early on
-;; (defun yas-add-to-dirs ( elt )
-;;   "Add ELT to `yas-snippet-dirs'"
-;;   (unless (listp yas-snippet-dirs)
-;;     (setq yas-snippet-dirs (list yas-snippet-dirs)))
-;;   (unless (member elt yas-snippet-dirs)
-;;     (if (null yas-snippet-dirs)
-;; 	(setq yas-snippet-dirs (list elt))
-;;       (push elt (cdr yas-snippet-dirs))))
-;;   (yas-load-directory elt t)
-;;   yas-snippet-dirs)
-
-;; ;;;
-;; ;;; Create needed auto execs so as things get loaded, items are
-;; ;;; cusomized as we want them.
-;; ;;;
-;; ;; Add our private recipe directory to el-get's
-;; (eval-after-load 'el-get
-;;   '(progn
-;;      (message "adding to el-get's recipes")
-;;      (add-to-list 'el-get-recipe-path (expand-file-name "recipes" user-emacs-directory))))
-
-;; ;; Lets go ahead and turn on yasnippet mode.
-;; (eval-after-load 'yasnippet
-;;   '(progn
-;;      (message "yas global mode")
-;;      (yas-global-mode 1)))
-
-;; ;; We want snippets in rspec mode
-;; (eval-after-load 'rspec-mode
-;;   '(progn
-;;      (message "calling rspec-install-snippets")
-;;      (yas-add-to-dirs rspec-snippets-dir)
-;;      (add-hook 'dired-mode-hook 'rspec-dired-mode)))
-
-;; ;; we want the snippets in feature-mode
-;; (eval-after-load 'feature-mode
-;;   '(progn
-;;      (message "adding feature snippet directory")
-;;      (require 'yasnippet)
-;;      (yas-add-to-dirs feature-snippet-directory)))
-
-;; Pull in forge when we use magit
-;; (with-eval-after-load 'magit
-;;   (require 'forge))
 (defun pedz-magit-set-sort-column ()
   (setq tabulated-list-sort-key (cons "Name" nil)))
 (eval-after-load 'magit
   (add-hook 'magit-repolist-mode-hook 'pedz-magit-set-sort-column))
-
-;; my recipes
-;; (setq el-get-sources
-;;       '((:name magit
-;;       	       :before (global-set-key (kbd "C-x C-z") 'magit-status))
-
-;; 	(:name cscope
-;; 	       :type github
-;; 	       :pkgname "pedz/cscope.el"
-;; 	       :post-init (add-hook 'c-initialization-hook
-;; 				    (lambda () (require 'cscope))))
-;; 	(:name js2-mode
-;; 	       :post-init (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
-;; 	(:name jquery-doc
-;; 	       :post-init (add-hook 'js2-mode-hook 'jquery-doc-setup))
-;; 	(:name gsa-cscope
-;; 	       :type github
-;; 	       :pkgname "pedz/gsa-cscope")
-;; 	))
-
-;; my packages
-;; (setq my-packages '(
-;; 		    ;; el-get must be first
-;; 		    el-get
-;; 		    ;; package needs to be second
-;; 		    package
-;; 		    ;; load yasnippet before any other package that
-;; 		    ;; adds to the snippet dir list.
-;; 		    yasnippet
-;; 		    ;; Rest of the list
-;; 		    auto-complete
-;; 		    ;; doxymacs
-;; 		    s
-;; 		    feature-mode
-;; 		    inf-ruby
-;; 		    rspec-mode
-;; 		    yari
-;; 		    ))
-
-;; Add rinari only if "bundle" is in our path or it won't install.
-;;
-;; This would not build last time I tried Thu Dec 29 18:50:13 CST 2016
-;;
-;; (if (executable-find "bundle")
-;;   (add-to-list 'my-packages 'rinari t))
-
-
-;;     git@github.dev.us-east-1.aws.galleon.c.statestr.com:e654643/tn-svc-orchestrate.git
-;; https://github.dev.us-east-1.aws.galleon.c.statestr.com/e654643/tn-svc-orchestrate/tree/pedz-pull-configure-out
-;; trim git@ and .git, prepend https://, change : to / (once?), append /tree/<branch>
 
 ;; Move the customizable values off to their own file
 ;; and load that file
@@ -136,30 +41,6 @@
 (add-to-list 'exec-path "/usr/local/bin")
 (add-to-list 'exec-path (expand-file-name "~/bin"))
 (setenv "PATH" (mapconcat 'identity exec-path ":"))
-
-;; (defun sync-packages ()
-;;   "Synchronize el-get packages"
-;;   (interactive)
-;;   (message "Starting sync")
-;;   (el-get 'sync (append my-packages 
-;; 			(mapcar 'el-get-as-symbol
-;; 				(mapcar 'el-get-source-name el-get-sources))))
-;;   (message "All packages are synchronized")
-;;   (require 'pedz))
-
-;; ;; This piece of code sucks over el-get if it is not present on the
-;; ;; system and then calls sync-packages which will suck over all the
-;; ;; other packages as well.
-;; (if (require 'el-get nil t)
-;;     (sync-packages)
-;;   (url-retrieve
-;;    "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
-;;    (lambda (s)
-;;      (let (el-get-master-branch)
-;;        (end-of-buffer)
-;;        (eval-print-last-sexp)
-;;        (setq el-get-verbose t)
-;;        (sync-packages)))))
 
 (require 'pedz)
 
@@ -177,9 +58,12 @@
 
 ;; Basic setup
 
-(el-get 'sync)
+;; (el-get 'sync)
 
 ;; Advanced setup
+
+(el-get-bundle magit
+  :before (global-set-key (kbd "C-x g") 'magit-status))
 
 ;; ;; Simple package names
 ;; (el-get-bundle yasnippet)
