11f12cb4741b3f9b6cbc285483ca90a659d1ea34
Perry Smith <pedz@easesoftware.com>
2022-04-18 21:21:26 -0500

    Added /opt/homebrew/share/info to Info-additional-directory-list

--- customize.el
+++ customize.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-additional-directory-list '("/opt/homebrew/share/info"))
  '(Info-default-directory-list
    (delq nil
          (mapcar
